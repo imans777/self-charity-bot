e53d12eff61987f3c0078e37b60674418e222108
imans777 <iman.s_sani@yahoo.com>
2018-12-29 04:58:18 +0330

    revert bonus week back

--- commands.js
+++ commands.js
@@ -8,7 +8,6 @@ const user = {
     firstname: '',
     lastname: '',
     username: '',
-    onSelf: false, // TODO: no need for this
     self: '',
     code: 0,
 };
@@ -33,15 +32,8 @@ module.exports = (bot) => {
 
     bot.on(buttons.send_code.command, msg => {
         // console.log('send code', msg.text);
-        // TODO: remove this if, as it's not needed
-        if (!users.find(el => el.id === msg.from.id)) {
-            users.push({
-                id: msg.from.id,
-                onSelf: true
-            });
-        }
-        bot.sendMessage(msg.from.id, decodeURI(messages.advanced.choose_self_advanced), { // TODO: change this to normal.choose_self
-            replyMarkup: bot.keyboard(replies.return_back, {resize: true}) // TODO: change this to selfs
+        bot.sendMessage(msg.from.id, messages.normal.choose_self, {
+            replyMarkup: bot.keyboard(replies.selfs, {resize: true})
         });
     });
 
@@ -78,49 +70,25 @@ module.exports = (bot) => {
     });
 
     bot.on('text', msg => {
-        // this is the bonus week code
-        let irrelevant = false;
-        Object.keys(buttons).forEach(el => {
-            if (msg.text === buttons[el].label || msg.text === buttons[el].command) {
-                irrelevant = true;
+        let self_found = false;
+        replies.selfs.forEach(el => {
+            if (self_found)
+                return;
+            if (el[0] === msg.text) {
+                self_found = true;
+                // console.log('got self, get code', msg.text);
+                users.push({
+                    id: msg.from.id,
+                    firstname: msg.from.first_name,
+                    lastname: msg.from.last_name || '',
+                    username: msg.from.username || '',
+                    self: msg.text
+                });
+                bot.sendMessage(msg.from.id, messages.normal.send_code, {
+                    replyMarkup: bot.keyboard(replies.return_back, {resize: true}), ask: 'get_code'
+                });
             }
         });
-        if (irrelevant) {
-            return;
-        }
-        let user = users.find(el => el.id === msg.from.id && el.onSelf === true);
-        if (!user)
-            return;
-        Object.assign(user, {
-            firstname: msg.from.first_name,
-            lastname: msg.from.last_name,
-            username: msg.from.username,
-            onSelf: false,
-            self: msg.text
-        });
-        bot.sendMessage(msg.from.id, messages.normal.send_code, {
-            replyMarkup: bot.keyboard(replies.return_back, {resize: true}), ask: 'get_code'
-        });
-        // TODO: this is the normal code. revert to this
-        // let self_found = false;
-        // replies.selfs.forEach(el => {
-        //     if (self_found)
-        //         return;
-        //     if (el[0] === msg.text) {
-        //         self_found = true;
-        //         // console.log('got self, get code', msg.text);
-        //         users.push({
-        //             id: msg.from.id,
-        //             firstname: msg.from.first_name,
-        //             lastname: msg.from.last_name || '',
-        //             username: msg.from.username || '',
-        //             self: msg.text
-        //         });
-        //         bot.sendMessage(msg.from.id, messages.normal.send_code, {
-        //             replyMarkup: bot.keyboard(replies.return_back, {resize: true}), ask: 'get_code'
-        //         });
-        //     }
-        // });
     });
 
     function isCodeValidated(code) {
